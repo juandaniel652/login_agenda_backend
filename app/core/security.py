1548b2e6505f75ee9ebe270718ae305b5d7bd2bf
Juan Daniel <juankycabj05@gmail.com>
2026-01-01 20:09:03 -0300

    requirements actualizado y security validado y protegido

--- app/core/security.py
+++ app/core/security.py
@@ -1,10 +1,34 @@
+from datetime import datetime, timedelta
 from passlib.context import CryptContext
+from jose import jwt
+
+# =========================
+# CONFIGURACIÓN GENERAL
+# =========================
+
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+if not SECRET_KEY:
+    raise RuntimeError("SECRET_KEY no configurada")
+
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
+from jose import jwt
+import os
+
 
 pwd_context = CryptContext(
     schemes=["argon2"],
     deprecated="auto"
 )
 
+# =========================
+# PASSWORDS
+# =========================
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
@@ -12,3 +36,24 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, hashed: str) -> bool:
     return pwd_context.verify(password, hashed)
+
+# =========================
+# JWT
+# =========================
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + (
+        expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
