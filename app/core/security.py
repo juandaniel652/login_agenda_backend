7d2a229360e35984408aec654bc20c1c41dcf9a7
Juan Daniel <juankycabj05@gmail.com>
2026-01-01 19:58:24 -0300

    De limitacion de bits en contraseña, a moderno y funcional

--- app/core/security.py
+++ app/core/security.py
@@ -1,43 +1,14 @@
-from datetime import datetime, timedelta
-from jose import jwt, JWTError
 from passlib.context import CryptContext
-from fastapi import HTTPException, status
-from app.core.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["argon2"],
+    deprecated="auto"
+)
 
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 
-def verify_password(password: str, password_hash: str) -> bool:
-    return pwd_context.verify(password, password_hash)
-
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(
-        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-    )
-    to_encode.update({"exp": expire})
-    return jwt.encode(
-        to_encode,
-        settings.JWT_SECRET_KEY,
-        algorithm=settings.JWT_ALGORITHM
-    )
-
-
-
-def decode_token(token: str) -> dict:
-    try:
-        return jwt.decode(
-            token,
-            settings.JWT_SECRET_KEY,
-            algorithms=[settings.JWT_ALGORITHM]
-        )
-    except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token inválido"
-        )
+def verify_password(password: str, hashed: str) -> bool:
+    return pwd_context.verify(password, hashed)
